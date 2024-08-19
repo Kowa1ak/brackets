54edf0bc33580f67c4658b72adabf4ed8c5edd7e
Danila <danila.tsatsuk@trackera.io>
2024-08-19 17:20:08 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+function check(str, bracketsConfig) {
+  const work = str.split("");
+  const stack = [];
+  for (const br of work) {
+    let good = bracketsConfig.filter((el) => el[0] === br)[0];
+    if (good && stack.length > 0) {
+      if (good[0] === stack[stack.length - 1][1]) {
+        stack.pop();
+        continue;
+      }
+    }
+    if (good) {
+      stack.push(good);
+    } else if (stack.length === 0) return false;
+    else {
+      good = stack.pop();
+      if (br !== good[1]) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
+
+module.exports = check;
